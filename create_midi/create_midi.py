ea9ff44ed1f8fcd62b767008d1bf9685672cc688
handav <hannahdavis0220@gmail.com>
2018-10-25 18:33:00 -0400

    initial commit

---
+++ create_midi/create_midi.py
@@ -0,0 +1,30 @@
+import os
+import midiutil
+
+def compose_midi(text):
+    track = 0
+    channel = 0
+    time = 0   # in beats
+    tempo = 120  # beats per minute
+    volume = 100 # from 0-127
+    midi_file = midiutil.MIDIFile(1, adjust_origin=True)
+    midi_file.addTempo(track, time, tempo)
+    for item in text.split():
+        note = ''.join(list(item)[:2])
+        note = int(note)
+        duration = ''.join(list(item)[2:])
+        duration = float(duration)
+        midi_file.addNote(track, channel, note, time, duration, volume)
+        time = time + duration
+    return midi_file
+
+def save_midi(cleaned_tweet, midi_file):
+    filename = 'sample_output.mid'
+    with open(filename, 'wb') as output_file:
+        midi_file.writeFile(output_file)
+        print 'midi file saved'
+
+with open('../output/notes.txt', 'r') as input_file:
+    notes = input_file.read()
+    midi_file = compose_midi(notes)
+    save_midi(notes, midi_file)
---
+++ generate_from_lstm_p5/main.js
@@ -0,0 +1,39 @@
+// See https://ml5js.org/docs/LSTMGenerator
+
+var lstm;
+var displayText;
+var all_generations = [];
+
+function preload() {
+    // Create the LSTM Generator with a pre trained model
+    lstm = ml5.LSTMGenerator('../models/spanish_input/');
+    console.log(lstm);
+}
+
+function setup() {
+    createCanvas(600, 400);
+}
+
+function draw() {
+    if (displayText) {
+        background(255);
+        text(displayText, 30, 30);
+    }
+}
+
+function keyPressed() {
+    // Generete content
+    lstm.generate({
+        seed: 'the meaning of pizza is',
+        length: 60,
+        temperature: 0.5
+    }, function(results) {
+        displayText = results.generated;
+        all_generations.push(displayText);
+        console.log(all_generations);
+    });
+    if (keyCode === ENTER) {
+        console.log('saving file');
+        saveStrings(all_generations, 'notes.txt');
+    }
+}
